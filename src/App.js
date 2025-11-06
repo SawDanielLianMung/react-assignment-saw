8ee90e75b0037d749e0625594f855ea1da85b690
SawDanielLianMung <munsd004@mymail.unisa.edu.au>
2025-11-07 05:19:51 +1030

    Update App.js

--- src/App.js
+++ src/App.js
@@ -118,9 +118,10 @@ return (
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-md-8" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
-                         <ProcessTexture/>
+                        <ProcessTexture/>
                     </div>
                     <div className="col-md-4">
+
                         <nav>
                             <ProcButtons/>
                             <br />
