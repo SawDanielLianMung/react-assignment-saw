26a30ff0210c5d112a3d3f4e171352681c745ecf
SawDanielLianMung <munsd004@mymail.unisa.edu.au>
2025-11-07 05:28:39 +1030

    Update SoundControl

--- src/components/SoundControl.js
+++ src/components/SoundControl.js
@@ -3,8 +3,11 @@ function SoundControl(){
         <>
             <div className="input-group mb-3">
                 <span className="input-group-text" id="cpm_label">SetCPM</span>
-                <input type="text" className="form-control" placeholder="120" aria-label="Username"
+                <input type="text" className="form-control" id="cpm_text_input" placeholder="120" aria-label="cpm"
                        aria-describedby="cpm_label"/>
+
+                <label htmlFor="volume_range" className="form-label">Volume</label>
+                <input type="range" className="form-range" min="0" max="1" step="0.01" id="volume_range"/>
             </div>
         </>
     );
